432cfc4f1f11aad4694914e692ecdbbf7b5a7572
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-07-18 14:27:55 -0300

    Queries Knex e Variáveis de Ambiente
    Aula testando algumas queries utilizando o Knex e aula sobre variáveis de ambiente. O arquivo .env fica no .gitignore por segurança, especialmente para chaves de API. Porém, documentamos as variáveis em outro arquivo para não gerar transtornos com outros Devs.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,27 @@
 import fastify from 'fastify';
+import crypto from 'node:crypto';
 import { knex } from './database';
 
 const app = fastify();
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*');
+  // const tables = await knex('sqlite_schema').select('*');
+  // return tables; Experimentando outras operações:
 
-  return tables;
+  // // Inserção:
+  // const transactions = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*'); // Returning faz a rota retornar os dados da transação, e não só um [1]
+
+  // Busca/select:
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*'); // Só o select * seleciona todos na busca
+  return transactions;
 });
 
 app.listen({ port: 3333 }).then(() => console.log('HTTP Server Running!'));
