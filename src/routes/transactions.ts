b31a4f2c01c29ba6f6b10426365ed2f1a2dde69b
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-08-01 11:00:35 -0300

    Resumo de transações e cookies no fastify
    Com o pacote "npm i @fastify/cookie" e modificações no server ANTES DAS ROTAS.

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -8,20 +8,26 @@ export async function transactionRoutes(app: FastifyInstance) {
   app.get('/', async () => {
     const transactions = await knex('transactions').select('*'); // Sem asterisco, vazio, ele entende tb
 
-    return { transactions }
+    return { transactions };
   });
 
   app.get('/:id', async (request) => {
     const getTransactionsParamsSchema = z.object({
       id: z.string().uuid(),
-    })
+    });
 
     const { id } = getTransactionsParamsSchema.parse(request.params);
 
     const transaction = await knex('transactions').where('id', id).first(); // Primeiro com id
 
-    return { transaction }
-  })
+    return { transaction };
+  });
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions').sum('amount', { as: 'amount' }).first(); // first() para entender que o retorno é um só; não um array
+
+    return { summary };
+  });
 
   app.post('/', async (request, reply) => {
     // Prefix no server já define o transactions depois da barra
@@ -36,10 +42,23 @@ export async function transactionRoutes(app: FastifyInstance) {
       request.body
     );
 
+    let sessionId = request.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days (clean code)
+        // maxAge ou expires com new Date(), para determinar a duração do cookie;
+      });
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1, // Débito fica negativo.
+      session_id: sessionId,
     });
 
     return reply.status(201).send();
