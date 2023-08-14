468f1f50e86f00ac46de0d1c530528c5f1520891
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-08-14 14:00:49 -0300

    Ajustes para Deploy no Render
    Só vou deixar o projeto com estes ajustes para poder fazer o Deploy no Render. Não farei o banco lá, porque só vai durar 90 dias. Portanto, este commit é mais para ter as configurações para esse tipo de deploy, como fazer a aplicação suportar não só o banco SQLite que utilizamos até aqui, mas também o PostgreSQL que o Render utiliza.

--- src/env/index.ts
+++ src/env/index.ts
@@ -9,8 +9,9 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'), // enum - conjunto de valores possíveis
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 });
 
 const _env = envSchema.safeParse(process.env);
